09faa273b00d5cff91f46fe37b919c750fb6ca60
Shawn Dreifuss <shawndreifuss@icloud.com>
2023-12-19 12:45:01 -0800

    added register user route

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -4,21 +4,23 @@ export const register = (req, res) => {
    // Check existing user 
    const q = 'SELECT * FROM users WHERE email = ? OR username = ?';
 
-   db.query(q, [req.body.email, req.body.username], (err, result) => {
-      if (err) {
-         return res.status(500).json({ error: err.message });
-      }
-      if (result.length > 0) {
+   db.query(q, [req.body.email, req.body.username], (err, data) => {
+      if(error) return res.json({ error: error.message });
+
+      if(data.length > 0) {
          return res.status(409).json({ error: 'User already exists' });
       }
-      // Create new user
-      const q = 'INSERT INTO users (username, email, password) VALUES (?, ?, ?)';
-      db.query(q, [req.body.username, req.body.email, req.body.password], (err, result) => {
-         if (err) {
-            return res.status(500).json({ error: err.message });
-         }
-         return res.status(201).json({ message: 'User created' });
-      });
+
+      // Create user
+
+      const q = 'INSERT INTO users SET ?';
+
+      db.query(q, req.body, (err, data) => {
+         if(error) return res.json({ error: error.message });
+
+         return res.json({ message: 'User created successfully' });
+      }
+      )
    });
 };
 
